400b27e5e2006d10b86ada8be9548ba9b2d9c43d
Brandon Chinn <brandonchinn178@gmail.com>
2022-11-23 22:05:29 -0800

    Upgrade tasty-autocollect

--- test/Main.hs
+++ test/Main.hs
@@ -1,17 +1,11 @@
--- TODO: autogenerate imports + test tree
--- https://github.com/brandonchinn178/tasty-autocollect/issues/38
+{- AUTOCOLLECT.MAIN
+custom_main = true
+strip_suffix = Test
+-}
 
 import Test.Tasty
 
-import qualified CheckedIO.CoreTest
-import qualified CheckedIO.EnvironmentTest
+{- AUTOCOLLECT.MAIN.imports -}
 
 main :: Main
-main = defaultMain test
-
-test :: TestTree
-test =
-  testGroup "checked-io" $
-    [ testGroup "CheckedIO.Core" CheckedIO.CoreTest.tasty_autocollect_tests
-    , testGroup "CheckedIO.Environment" CheckedIO.EnvironmentTest.tasty_autocollect_tests
-    ]
+main = defaultMain $ testGroup "checked-io" {- AUTOCOLLECT.MAIN.tests -}
